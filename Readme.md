ceda4c587486ca7a345e1e81fce3b7534647eba6
Mr-Ojii <okaschan@gmail.com>
2020-10-26 13:13:10 +0900

    init commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Easy-POST
+**POST送信をお手軽に**  
+というのがコンセプト(?)  
+Sendボタンをクリックすると、テキストボックスの内容を元にJSONを作成し、指定されたURLにPOST送信します。  
+使用するためには、JavaScriptを有効にしてください。
+
+# Link
+[https://Mr-Ojii.github.io/Easy-POST](https://mr-ojii.github.io/Easy-POST)
---
+++ click.js
@@ -0,0 +1,106 @@
+function post() {
+    let data = {};
+    for (let index = 0; index < formcount; index++)
+    {
+        let str = document.getElementById("name" + index).value;
+
+        if (str != "")
+        {
+            data[str] = String.raw`${document.getElementById("value" + index).value}`;
+        }
+    }
+    let urlstr = document.getElementById("url").value;
+    if (urlstr != "")
+    {
+        const xml = new XMLHttpRequest();
+        xml.open("POST", urlstr, false);
+        xml.setRequestHeader("content-type", "application/json;charset=UTF-8");
+        let datastr = JSON.stringify(data);
+        datastr = datastr.replace(String.raw`\\n`, String.raw`\n`);
+        datastr = datastr.replace(String.raw`\\\n`, String.raw`\\\\n`);
+        console.log(datastr);
+        xml.send(datastr);
+        console.log(datastr);
+    }
+}
+
+function update()
+{
+    let text = "";
+    for (let index = 0; index < formcount; index++) {
+        text = text + 'name:<input type="text" id="name' + index + '"/>value:<input type="text" id="value' + index + '"/><br />';
+        document.getElementById("divbody").innerHTML = text;
+    }
+}
+
+function addcount()
+{
+    let tmp = getarray();
+
+    formcount += 1;
+    update();
+
+    setarray(tmp);
+}
+
+function minuscount()
+{
+    if (formcount > 1) {
+        let tmp = getarray();
+
+        formcount -= 1;
+        update();
+
+        setarray(tmp);
+    }
+}
+
+function clearcount()
+{
+    formcount = 1;
+    update();
+}
+
+function getarray()
+{
+    let tmp = [];
+    for (let index = 0; index < formcount; index++) {
+        tmp[index] = [];
+        tmp[index].name = document.getElementById("name" + index).value;
+        tmp[index].value = document.getElementById("value" +
+            index).value;
+    }
+    return tmp;
+}
+
+function setarray(tmp)
+{
+    for (index = 0; index < Math.min(tmp.length, formcount); index++) {
+        document.getElementById("name" + index).value = tmp[index].name;
+        document.getElementById("value" + index).value = tmp[index].value;
+    }
+}
+
+function template(str)
+{
+    let templatearray = [];
+    switch (str)
+    {
+        case "discord":
+            templatearray = discord;
+            break;
+        default:
+            return;
+    }
+
+    formcount = templatearray.length;
+    update();
+    setarray(templatearray);
+}
+
+const discord = [
+    { name : "content", value : "message" },
+    { name : "username", value : "username" },
+    { name : "avatar_url", value : "" }];
+
+let formcount = 1;
